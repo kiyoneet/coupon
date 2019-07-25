a66dcca7376865a8581dd045dacda86efc6cf71c
Yasumasa Takai <m.e.t.e.o.r.07@gmail.com>
2019-07-25 19:05:52 +0900

    add fiter by title

--- src/lambda/handlers/coupons/coupon-list.ts
+++ src/lambda/handlers/coupons/coupon-list.ts
@@ -1,10 +1,38 @@
-exports.handler = async function(event: any) {
+import { APIGatewayEvent } from 'aws-lambda';
+import { CouponDynamodbTable } from './../../infrastructures/coupons/coupon-dynamodb-table';
+import { stringLiteral } from '@babel/types';
+
+exports.handler = async function(event: APIGatewayEvent) {
   console.log('request:', JSON.stringify(event, undefined, 2));
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/plain'
-    },
-    body: `Hello, CDK! You've hit ${event.path}`
-  };
+  try {
+    if (event.queryStringParameters === null) {
+      const result = await CouponDynamodbTable.list();
+      return {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(result)
+      };
+    } else {
+      const title = event.queryStringParameters.title;
+      const result = await CouponDynamodbTable.getByTitle(title);
+      return {
+        statusCode: 200,
+        header: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(result)
+      };
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: 'Internal server error' })
+    };
+  }
 };
--- src/lambda/infrastructures/coupons/coupon-dynamodb-table.ts
+++ src/lambda/infrastructures/coupons/coupon-dynamodb-table.ts
@@ -2,7 +2,7 @@ import * as AWS from 'aws-sdk';
 import { CouponItem } from '../../domains/coupons/coupon';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
-const TABLE_NAME = process.env.TABLE_NAME;
+const TABLE_NAME = process.env.TABLE_NAME!;
 const dynamoClient: DocumentClient = new AWS.DynamoDB.DocumentClient();
 
 export class CouponDynamodbTable {
@@ -11,7 +11,7 @@ export class CouponDynamodbTable {
 id: string : Promise<Coupon>    */
   public static async getCoupnById(id: string): Promise<CouponItem> {
     const param: DocumentClient.GetItemInput = {
-      TableName: TABLE_NAME!,
+      TableName: TABLE_NAME,
       Key: { id }
     };
     const record: DocumentClient.GetItemOutput = await dynamoClient
@@ -20,13 +20,40 @@ id: string : Promise<Coupon>    */
     return record.Item as CouponItem;
   }
 
-  //   /**
-  //    * scanCoupons
-  //    */
-  //   public static async scanCoupons(lastEvalationKey?: {}): Promise<any> {
-  //     const data =  await mapper.scan(Coupon);
-  //     if (data.)
-  //   }
-
-  //   public static async filterByTitle(title: string) {}
+  public static async getByTitle(title: string): Promise<any> {
+    const param: DocumentClient.QueryInput = {
+      TableName: TABLE_NAME,
+      IndexName: 'title',
+      KeyConditionExpression: '#p = :hkey',
+      ExpressionAttributeNames: {
+        '#p': 'title'
+      },
+      ExpressionAttributeValues: {
+        ':hkey': title
+      }
+    };
+    const record: DocumentClient.QueryOutput = await dynamoClient
+      .query(param)
+      .promise();
+    console.log(record);
+    return record.Items;
+  }
+  public static async list(): Promise<any> {
+    const param: DocumentClient.ScanInput = {
+      TableName: TABLE_NAME
+    };
+    let records: DocumentClient.ScanOutput = await dynamoClient
+      .scan(param)
+      .promise();
+    let items = records.Items;
+    while (records.LastEvaluatedKey) {
+      records = await dynamoClient
+        .scan(
+          Object.assign(param, { ExculusiveStartKey: records.LastEvaluatedKey })
+        )
+        .promise();
+      items = items!.concat(records.Items!);
+    }
+    return items;
+  }
 }
